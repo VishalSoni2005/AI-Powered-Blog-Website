b6d9ddc66c9b3be235cf7552ecc4230413de1ca8
VishalSoni007 <vsoni0882@gmail.com>
2025-02-18 17:44:54 +0530

    Trenging Blog Pages

--- client/src/components/blog-post.component.jsx
+++ client/src/components/blog-post.component.jsx
@@ -1,38 +1,52 @@
-import React from 'react'
-import { getDay } from '../common/date';
+import React from "react";
+import { getDay } from "../common/date";
+import { Link } from "react-router-dom";
 
 export default function BlogPostCard({ content, author }) {
+  let {
+    publishedAt,
+    tags,
+    title,
+    des,
+    banner,
+    activity: { total_likes },
+    blog_id: id
+  } = content;
 
-    let { publishedAt,
-        tags,
-        title,
-        des,
-        banner,
-        activity: { total_liken },
-        blog_id: id
-    } = content;
+  let { fullname, profile_img, username } = author;
+  return (
+    //todo:
+    <Link to={`/blog/${id}`} className="border-grey mb-4 flex items-center gap-8 border-b pb-5">
+      {/* 
+        //? whenever i click on any element of this div it will redirect me to blog page of blog */}
+      <div className="w-full">
+        <div className="mb-7 flex items-center gap-2">
+          <img src={profile_img} className="h-6 w-6 rounded-full" />
 
-    let { fullname, profile_img, username } = author;
-    return (
-        <div className='w-full'>
-
-            <div className='flex gap-2 items-center mb-7'>
-                <img src={profile_img} className='w-6 h-6 rounded-full' />
-
-                <p className='line-clamp-1'>{fullname} @{username}</p>
-                <p className='min-w-fit'>{getDay(publishedAt)}</p>
-
-            </div>
-
-
-
-
-
-            <h1 className='blog-title'>{title}</h1>
-
-            <p className='my-3 text-xl font-gelasio leading-7 max-sm:hidden md:max-[1100px]:hidden line-clamp-2'>{des}</p>
-
-
+          <p className="line-clamp-1">
+            {fullname} @{username}
+          </p>
+          <p className="min-w-fit">{getDay(publishedAt)}</p>
         </div>
-    )
+
+        <h1 className="blog-title">{title}</h1>
+
+        <p className="font-gelasio my-3 line-clamp-2 text-xl leading-7 max-sm:hidden md:max-[1100px]:hidden">
+          {des}
+        </p>
+
+        <div className="mt-7 flex gap-4">
+          <span className="btn-light px-4 py-1">{tags[0]}</span>
+
+          <span className="text-dark-grey ml-3 flex items-center gap-2">
+            <i className="fi fi-rr-heart text-xl"></i>
+            {total_likes}
+          </span>
+        </div>
+      </div>
+      <div className="bg-grey aspect-square h-28">
+        <img src={banner} className="aspect-square h-full w-full object-cover" alt="" />
+      </div>
+    </Link>
+  );
 }
--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -1,65 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import AnimationWrapper from '../common/page-animation'
-import axios from 'axios';
-import InPageNavigation from '../components/inpage-navigation.component'
+import React, { useEffect, useState } from "react";
+import AnimationWrapper from "../common/page-animation";
+import axios from "axios";
+import InPageNavigation from "../components/inpage-navigation.component";
 import Loader from "../components/loader.component";
-import BlogPostCard from '../components/blog-post.component';
+import BlogPostCard from "../components/blog-post.component";
 
 export default function HomePage() {
-
   let [blogs, setBlogs] = useState(null);
+  // let []
 
   useEffect(() => {
     getLatestBlogs();
-  }, [])
+  }, []);
 
-  const getLatestBlogs = async () => {  // use this funciton in useEffect to get latest blogs 
+  const getLatestBlogs = async () => {
+    // use this funciton in useEffect to get latest blogs
     try {
-      const latestBlog = await axios.get("http://localhost:3000/latest-blogs")
+      const latestBlog = await axios.get("http://localhost:3000/latest-blogs");
 
       const blogsArray = latestBlog.data.blogs;
 
-      setBlogs(blogsArray);   //! blog from backend is stored to useState form here
+      setBlogs(blogsArray); //! blog from backend is stored to useState form here
       // console.log("blogsArray => ", blogsArray);
-      console.log("blogsArray => ", blogs); // show value after first render  
+      console.log("blogsArray => ", blogs); // show value after first render
+    } catch (err) {
+      console.error("Error fetching latest blogs", err);
     }
-    catch (err) {
-      console.error("Error fetching latest blogs", err)
-    }
-  }
+  };
+
+  //!start from here
+  // const getTrendingBlogs = async () => {
+  //   // use this funciton in useEffect to get latest blogs
+  //   try {
+  //     const latestBlog = await axios.get("http://localhost:3000/trending-blogs");
+
+  //     const blogsArray = latestBlog.data.blogs;
+
+  //     setBlogs(blogsArray); //! blog from backend is stored to useState form here
+  //     // console.log("blogsArray => ", blogsArray);
+  //     console.log("blogsArray => ", blogs); // show value after first render
+  //   } catch (err) {
+  //     console.error("Error fetching latest blogs", err);
+  //   }
+  // };
   return (
     <AnimationWrapper>
-      <section className='h-cover flex justify-center gap-10'>
+      <section className="h-cover flex justify-center gap-10">
         {/* latest blogs */}
-        <div className='w-full '>
+        <div className="w-full">
           <InPageNavigation defaultHidden={["trending blogs"]} routes={["home", "trending blogs"]}>
-
-
             <>
-              {
-                blogs == null ?
-                  <Loader />
-                  :
-                  blogs.map((blog, i) => {
-                    return (
-                      <AnimationWrapper transition={{ duration: 1, delay: i * 0.1 }} key={i}>
-
-                        <BlogPostCard content={blog} author={blog.author.personal_info}  />
-                      </AnimationWrapper>
-                    )
-                  })
-
-
-              }
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper transition={{ duration: 1, delay: i * 0.1 }} key={i}>
+                      <BlogPostCard content={blog} author={blog.author.personal_info} />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
             </>
           </InPageNavigation>
         </div>
 
         {/* popular blogs and filter */}
-        <div>
-
-        </div>
+        <div></div>
       </section>
     </AnimationWrapper>
-  )
+  );
 }
