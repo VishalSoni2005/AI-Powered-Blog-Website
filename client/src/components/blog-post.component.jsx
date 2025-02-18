03e64bd6a94df29961d98e2101f7720578b27b5a
VishalSoni007 <vsoni0882@gmail.com>
2025-02-18 16:56:08 +0530

    Blog Card Component

--- client/src/components/blog-post.component.jsx
+++ client/src/components/blog-post.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { getDay } from '../common/date';
 
 export default function BlogPostCard({ content, author }) {
 
@@ -13,6 +14,25 @@ export default function BlogPostCard({ content, author }) {
 
     let { fullname, profile_img, username } = author;
     return (
-        <div>BlogPostCard</div>
+        <div className='w-full'>
+
+            <div className='flex gap-2 items-center mb-7'>
+                <img src={profile_img} className='w-6 h-6 rounded-full' />
+
+                <p className='line-clamp-1'>{fullname} @{username}</p>
+                <p className='min-w-fit'>{getDay(publishedAt)}</p>
+
+            </div>
+
+
+
+
+
+            <h1 className='blog-title'>{title}</h1>
+
+            <p className='my-3 text-xl font-gelasio leading-7 max-sm:hidden md:max-[1100px]:hidden line-clamp-2'>{des}</p>
+
+
+        </div>
     )
 }
