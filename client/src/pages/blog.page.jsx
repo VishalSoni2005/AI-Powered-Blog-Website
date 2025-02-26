0c1eaf04c5f18f137465715e12d0e0acb284bb38
VishalSoni007 <vsoni0882@gmail.com>
2025-02-26 17:30:41 +0530

    Blog page progress

--- client/src/pages/blog.page.jsx
+++ client/src/pages/blog.page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export const blogStructure = {
+  title: "",
+  des: "",
+  banner: "",
+  content: [],
+  tags: [],
+  author: { personal_info: {} },
+  publishedAt: ""
+};
 
 export default function BlogPage() {
-    let { blog_id } = useParams();
-  return (
-    <div>BlogPage {blog_id}</div>
-  )
+  let { blog_id } = useParams();
+
+  const [blog, setBlog] = useState(null);
+
+  let {
+    title,
+    des,
+    banner,
+    content,
+    author: {
+      personal_info: { fullname, username, profile_img }
+    },
+    publishedAt
+  } = blog;
+
+  const fetchBlog = () => {
+    try {
+      const request = axios.post("http://localhost/3000/get-blog", { blog_id });
+      const data = request.data.blog;
+      setBlog(data);
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+  return <div>BlogPage {blog.title}</div>;
 }
